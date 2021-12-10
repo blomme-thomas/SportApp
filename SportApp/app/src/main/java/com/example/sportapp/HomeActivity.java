5262d3fabb9968bb7c91041e24b73df0f9e0cfd8
Thomas <blommethomas59@gmail.com>
2021-12-10 05:07:50 +0100

    bug layout

--- SportApp/app/src/main/java/com/example/sportapp/HomeActivity.java
+++ SportApp/app/src/main/java/com/example/sportapp/HomeActivity.java
@@ -37,7 +37,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
