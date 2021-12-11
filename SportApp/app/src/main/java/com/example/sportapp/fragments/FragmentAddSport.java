3b4dc51cd98b150c4c499c24f6caa65dd7dfb0cc
Thomas <blommethomas59@gmail.com>
2021-12-11 01:30:07 +0100

    ajout de sports

--- SportApp/app/src/main/java/com/example/sportapp/fragments/FragmentAddSport.java
+++ SportApp/app/src/main/java/com/example/sportapp/fragments/FragmentAddSport.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.sportapp.R;
 import com.example.sportapp.User;
@@ -17,6 +18,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class FragmentAddSport extends Fragment {
 
@@ -26,6 +29,9 @@ public class FragmentAddSport extends Fragment {
     private FirebaseUser user;
     private DatabaseReference reference;
     private String userID;
+
+    private EditText sport,level;
+    private ArrayList<String> sports,levels;
 
     public FragmentAddSport(User user_) {
         this.userProfile = user_;
@@ -43,15 +49,29 @@ public class FragmentAddSport extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_sport, container, false);
 
+        sport = (EditText) view.findViewById(R.id.sport);
+        level = (EditText) view.findViewById(R.id.level);
         addSport = (Button) view.findViewById(R.id.addSport);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users");
         userID = user.getUid();
 
+
         addSport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String sportText = sport.getText().toString().trim();
+                String levelText = level.getText().toString().trim();
+
+                sports = userProfile.sports;
+                sports.add(sportText);
+                levels = userProfile.levels;
+                levels.add(levelText);
+
+                reference.child(userID).child("sports").setValue(sports);
+                reference.child(userID).child("levels").setValue(levels);
+
                 FragmentManager frman = getFragmentManager();
                 FragmentTransaction ftran = frman.beginTransaction();
                 Fragment ffrag = new FragmentProfil(userProfile);
